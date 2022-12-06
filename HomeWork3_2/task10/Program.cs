d4677dc95e0c95c9ddf0ac48c6d02fb7efa5bd6c
GumarGarifullyn <118556134+GumarGarifullyn@users.noreply.github.com>
2022-12-06 20:27:56 +0300

    add task10

--- HomeWork3_2/task10/Program.cs
+++ HomeWork3_2/task10/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и на выходе показывает вторую цифру этого
+// числа.
+// 456 -> 5
+// 782 -> 8
+// 918 -> 1
+
+Console.WriteLine("Введите трехзначное число: ");
+int N = int.Parse(Console.ReadLine()!);
+if(N>99 && N<1000) 
+{
+    Console.WriteLine((N%100)/10);
+}
+else 
+{
+    Console.WriteLine("Вы ввели не правильное число");
+}
