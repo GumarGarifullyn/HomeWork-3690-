cb1455fea8dfef2fa84d27ae1fcc6fbd2a6059c6
GumarGarifullyn <118556134+GumarGarifullyn@users.noreply.github.com>
2022-12-08 15:27:26 +0300

    add task23

--- HomeWork3_3/task23/Program.cs
+++ HomeWork3_3/task23/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите число N");
+int N = int.Parse(Console.ReadLine()!);
+int count = 1;
+Console.Write($"{N} -> ");
+while(count < N)
+{
+    Console.Write($"{Math.Pow(count,3)}, ");
+    count = count + 1;
+}
+if(count == N)
+{
+    Console.Write($"{Math.Pow(count,3)}");
+}
