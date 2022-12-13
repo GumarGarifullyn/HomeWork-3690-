455b7d55fa1bd5b6b1f5041d8e0aec85244c4d1e
GumarGarifullyn <118556134+GumarGarifullyn@users.noreply.github.com>
2022-12-14 01:16:33 +0300

    add task 25

---
+++ HomeWork3_4/task25/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход
+// два числа (A и B) и возводит число A в натуральную
+// степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+double Exponent(double numA, int numB)
+{
+    double result = 1;
+    for (int i = 1; i <= numB; i++)
+    {
+        result = result*numA;
+    }
+    return result;
+
+}
+Console.WriteLine("Введите число");
+double a = double.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите степень в которую необходимо возвести число");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine($"{a} в степени {b} -> {Exponent(a,b)}");
+
