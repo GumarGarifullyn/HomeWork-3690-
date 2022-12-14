7408d2dcc828d65f542fdba5c2ee160b4e9314b7
GumarGarifullyn <118556134+GumarGarifullyn@users.noreply.github.com>
2022-12-14 10:45:51 +0300

    add task 27

---
+++ HomeWork3_4/task27/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 27: Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int Sum(int number)
+{
+    int count = 1;
+    int n = number;
+
+    while(n > 0)
+    {
+        //Console.WriteLine($"{n} -> {n/10} -> {count}");
+        n = n/10;
+        count++;
+    }
+    int sum = 0;
+    while(count > 1)
+    {
+        sum = sum + number%10;
+        number = number/10;
+        count--;
+    }
+    return sum;
+}
+Console.WriteLine("Введите число");
+int num = int.Parse(Console.ReadLine()!);
+Console.WriteLine(Sum(num));
