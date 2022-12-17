b2a397de37a517655b86e77283d1460d41a830aa
GumarGarifullyn <118556134+GumarGarifullyn@users.noreply.github.com>
2022-12-18 01:11:49 +0300

    add task38

---
+++ HomeWork3_5/task38/Program.cs
@@ -0,0 +1,47 @@
+﻿// Задача 38: Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+// [3 7 22 2 78] -> 76
+
+double[] Array(int size)
+{
+    double[] array = new double[size];
+    for(int i = 0; i < size; i++)
+    {
+        array[i] = Convert.ToDouble(new Random().Next(-100, 1000)/10.0);
+    }
+        return array;
+}
+
+void PrintArray(double[] arr1)
+{
+    for(int i = 0; i < arr1.Length; i++)
+    {
+    if(i == 0) Console.Write($"[{arr1[i]}; ");
+    if(i == arr1.Length-1) Console.Write($"{arr1[i]}]");
+    if(i!=0 && i!=arr1.Length-1) Console.Write($"{arr1[i]}; ");
+    }
+}
+
+void Diff(double[] inArray)
+{
+    double max = inArray[0];
+    double min = inArray[0];
+    // for(int i = 0; i < inArray.Length; i++)
+    // {
+    //     if (inArray[i] > max) max = inArray[i];
+    
+    //     if(inArray[i] < min) min = inArray[i];
+    // }
+    foreach (double el in inArray)
+    {
+        if(el > max) max = el;
+        if(min > el) min = el;
+    }
+
+    Console.Write(" -> " + Math.Round((max-min),1));
+  }
+ 
+int size = new Random().Next(3,7);
+double[] array = Array(size);
+PrintArray(array);
+Diff(array);
